36d7dfa5dc33a3f8ffd65d47981c44030e636747
IrinaEnotova <irinkayashina@gmail.com>
2023-10-10 15:01:33 +0400

    refactor: create hook for saving and reading data in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,21 @@ import Header from "./components/Header/Header";
 import JournalList from "./components/JournalList/JournalList";
 import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
 import JournalForm from "./components/JournalForm/JournalForm";
-import { useEffect, useState } from "react";
+import { useLocalStorage } from "./hooks/useLocalStorage.hook";
+
+function mapItems(data) {
+  if (!data) return [];
+  return data.map((i) => ({ ...i, date: new Date(i.date) }));
+}
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem("data"));
-    if (items) {
-      setData(items.map((item) => ({ ...item, date: new Date(item.date) })));
-    }
-  }, []);
-
-  useEffect(() => {
-    if (data.length) {
-      localStorage.setItem("data", JSON.stringify(data));
-    }
-  }, [data]);
+  const [data, setData] = useLocalStorage("data");
 
   const addItem = (item) => {
-    setData((oldData) => [
-      ...oldData,
+    setData([
+      ...mapItems(data),
       {
-        id: oldData.length !== 0 ? Math.max(...oldData.map((el) => el.id)) + 1 : 1,
+        id: data.length !== 0 ? Math.max(...data.map((el) => el.id)) + 1 : 1,
         title: item.title,
         date: new Date(item.date),
         text: item.text,
@@ -41,7 +33,7 @@ function App() {
       <LeftPanel>
         <Header />
         <JournalAddButton />
-        <JournalList data={data} />
+        <JournalList data={mapItems(data)} />
       </LeftPanel>
       <Body>
         <JournalForm addItem={addItem} />
