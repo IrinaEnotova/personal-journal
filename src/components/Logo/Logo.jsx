7e636f39330447513c690c4c4c256d29b141f3b9
IrinaEnotova <irinkayashina@gmail.com>
2023-10-12 13:14:06 +0400

    feat: add build version

--- src/components/Logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-import styles from "./Logo.module.css";
-
-const Logo = ({ image }) => {
-  return <img className={styles.logo} src={image} alt="Logo of Journal" />;
-};
-
-export default memo(Logo);
